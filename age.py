e31cfa3b8d1487e19865585c92f71f87911bc9c2
unknown <Administrator@BG4WS0876.SONATA.LOCAL>
2021-09-28 13:02:00 +0530

    all files

---
+++ age.py
@@ -0,0 +1,6 @@
+no1=input('enter the name')
+no2=int(input('enter the age'))
+no3=int(input('enter the present year'))
+no4=int((no3-no2)+100)
+print("the year at which age will be 100 is",no4)
+
---
+++ comaprison.py
@@ -0,0 +1,10 @@
+no1 = int(input('enter the no1'))
+no2 = int(input('enter the no2'))
+no3 = int(input('enter the no3'))
+if (no1 >= no2) and (no1 >= no3):
+   largest = no1
+elif (no2 >= no1) and (no2 >= no3):
+   largest = no2
+else:
+   largest = no3
+print("The largest number is", large)
